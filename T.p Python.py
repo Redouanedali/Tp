aad9ef2e0c2b66a22f9e87de3f8ec298a69b9a72
Redouane <Redouane@redouane.fr>
2018-04-19 14:51:49 +0200

    first commit

---
+++ T.p Python.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""""
+#Exo01.py
+
+text = "Je dois faire des sauvegardes regulieres de mes fichiers"
+print(text * 500)
+
+
+
+Exo03.py
+#While
+i = 0
+nbr = 13
+while i < 11:
+    print (i * nbr)
+    i = i +1
+
+
+
+#Exo05.py
+
+a = int(input( " Give me a number : "))
+if (a % 2 == 0):
+    print("pair")
+else:
+    print("impair")
+
+
+#Exo04.py
+
+word = input("Give me a word")
+print (len(word))
+
+
+#Exo02
+
+r = 1
+while r < 1000 :
+    print (r)
+    r += 2
+
+"""
+
+
+
